6a3b9f7783932555726829658d6ca87f5effec77
Oraclesid1411 <giloracle76@gmail.com>
2025-06-24 18:07:50 +0000

    elio  update

--- components/vlog/all_Vlogs.jsx
+++ components/vlog/all_Vlogs.jsx
@@ -20,17 +20,19 @@ import TikTokEmbed from '../TikTokEmbed.jsx';
 
 function All_Vlogs() {
 
-
+  const [vlogTypes, setVlogTypes] = useState([]);
+  // const [filterType, setFilterType] = useState('');
+  
   const [Allvlog , setAllvlog] = useState([]);
 
-  console.log("Allvlog")
-  console.log(Allvlog)
+  // console.log("Allvlog")
+  // console.log(Allvlog)
  
-  const [actus , setActus] = useState([]);
-  const [lives , setLives] = useState([]);
-  const [castings , setCastings] = useState([]);
-  const [defiles , setDefiles] = useState([]);
-  const [interviews , setInterviews] = useState([]);
+  // const [actus , setActus] = useState([]);
+  // const [lives , setLives] = useState([]);
+  // const [castings , setCastings] = useState([]);
+  // const [defiles , setDefiles] = useState([]);
+  // const [interviews , setInterviews] = useState([]);
   
   const getEmbeddedUrl = (path, source) => {
     if (source === 2) {
@@ -56,6 +58,21 @@ function All_Vlogs() {
     return path;
   };
   
+  
+// useEffect(() => {
+//   const fetchTypes = async () => {
+//     try {
+//       const res = await axios.get(`${apiUrl}/vlogtypes`);
+//       // Ajouter "Tous" en premier
+//       setVlogTypes([{ id: '', libelle: 'Tous', slug: 'all' }, ...res.data]);
+//     } catch (err) {
+//       console.error("Erreur récupération types :", err);
+//     }
+//   };
+
+//   fetchTypes();
+// }, []);
+
   // function getEmbeddedUrl(path) {
   //   const youtubeMatch = path.match(/(?:v=|youtu\.be\/)([a-zA-Z0-9_-]+)/);
   //   if (youtubeMatch) {
@@ -69,239 +86,91 @@ function All_Vlogs() {
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const [rep , rep1, rep2, rep3, rep4, rep5] = await Promise.all([
+          const [rep , rep1] = await Promise.all([
             axios.get(`${apiUrl}/vlogs/all`),
-            // axios.get(`${apiUrl}/vlogs/actus`),
-            // axios.get(`${apiUrl}/vlogs/lives`),
-            // axios.get(`${apiUrl}/vlogs/castings`),
-            // axios.get(`${apiUrl}/vlogs/defiles`),
-            // axios.get(`${apiUrl}/vlogs/interviews`),
-          ]);
+            axios.get(`${apiUrl}/vlogs/vlog_type`),
+           ]);
 
           setAllvlog(rep.data?.vlogs)
-          // setActus(rep1.data);
-          // setLives(rep2.data);
-          // setCastings(rep3.data);
-          // setDefiles(rep4.data);
-          // setInterviews(rep5.data);
-        } catch (err) {
+          setVlogTypes([{ id: '', libelle: 'Tous', slug: 'all' }, ...rep1.data]);
+         } catch (err) {
           console.error("Erreur lors de la récupération des données :", err);
         }
       };
       fetchData();
     }, []);
 
+    // console.log("vlogTypes")
+    // console.log(vlogTypes)
+
   return (
     <>
                 <div className="container tab_list_box mb-3">
                     <div className="tab-list product-tab-list sticky_nav">
-                        <nav className="nav product-tab-nav">
-                        <a className="product-tab-link tab-link active" href="#All" data-bs-toggle="tab">Tous</a>
-                        <a className="product-tab-link tab-link" href="#Actus" data-bs-toggle="tab">Actus</a>
-                            <a className="product-tab-link tab-link" href="#Lives" data-bs-toggle="tab">Lives</a>
-                            <a className="product-tab-link tab-link " href="#Castings" data-bs-toggle="tab">Castings</a>
-                            <a className="product-tab-link tab-link " href="#Defiles" data-bs-toggle="tab">Défilés</a>
-                            <a className="product-tab-link tab-link " href="#Interviews" data-bs-toggle="tab">Interviews</a>
-                            {/* <a className="product-tab-link tab-link " href="#pstyle" data-bs-toggle="tab">nouveaux</a> */}
-                      </nav>
+                    <nav className="nav product-tab-nav">
+  {vlogTypes.map((type, index) => (
+    <a
+      key={type.slug || `type-${index}`}
+      className={`product-tab-link tab-link ${index === 0 ? 'active' : ''}`}
+      href={`#${type.slug}`}
+      data-bs-toggle="tab"
+    >
+      {type.libelle}
+    </a>
+  ))}
+</nav>
                     </div>
-                    <div className="tab-content product-tab-content col-12">
-                    <div id="All" className="tab-pane fade active show">
-                        <div className="row">
-
-                        {Allvlog.map((item) => (
-                                                      
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
+                    <div className="tab-content">
+  {vlogTypes.map((type, index) => (
+    <div
+      key={type.slug}
+      className={`tab-pane fade ${index === 0 ? 'show active' : ''}`}
+      id={type.slug}
+    >
+      <div className="row">
+        {Allvlog
+          .filter(v => !type.id || v.type_vlog === type.id)
+          .map(v => (
+                                                                  
+            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={v?.id} data-aos="fade-up" data-aos-duration="700">
                                                    
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                        
+            <div className="product-card video_card">
+                <div className="product-card-img">
+        
 
-                                                          <a
-                                                          className="hover-switch clip_video"
-                                                          data-fancybox="video-gallery"
-                                                          data-caption={item?.titre}
-                                                          data-type="iframe"
-                                                          href={getEmbeddedUrl(item?.path, item?.source)}
-                                                        >
-                                                          <img
-                                                            src={item?.thumbnail || "/default-thumbnail.jpg"}
-                                                            alt="video"
-                                                            loading="lazy"
-                                                            className="img"
-                                                          />
-                                                          <button className="play-button">▶</button>
-                                                        </a>
-                                                        
- 
-                                                              
+          <a
+          className="hover-switch clip_video"
+          data-fancybox="video-gallery"
+          data-caption={v?.titre}
+          data-type="iframe"
+          href={getEmbeddedUrl(v?.path, v?.source)}
+        >
+          <img
+            src={v?.thumbnail || "/default-thumbnail.jpg"}
+            alt="video"
+            loading="lazy"
+            className="img"
+          />
+          <button className="play-button">▶</button>
+        </a>
+        
 
-                                                                </div>
-                                                                <div className="card-title">
-                                                                  <label>{item?.titre} </label>
-                                                                </div>
+              
 
-                                                            </div>
-                                                            </div>
-                                              
-                                                      ))}
-                                                        
-                             </div>
-                        </div>
-                       <div id="Actus" className="tab-pane fade ">
-                        <div className="row">
+                </div>
+                <div className="card-title">
+                  <label>{v?.titre} </label>
+                </div>
 
-                        {actus.map((item) => (
-                                                        <>
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
-                                                   
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                                    <a className="hover-switch"
-                                                                      data-fancybox="video-gallery"
-                                                                      href={item?.path}
-                                                                      data-caption="Video 1">
-                                                                        
-                                                                    <img src={item?.thumbnail} alt="video" className='img'/>
-                                                                    <button className="play-button">▶</button>
-                                                                        
-                                                                    </a>
-                                                                
-                                                                </div>
+            </div>
+            </div>
+            // <VideoCard key={v.id} data={v} />
+          ))}
+      </div>
+    </div>
+  ))}
+</div>
 
-                                                            </div>
-                                                            </div>
-                                                            </>
-                                                      ))}
-                                                        
-                             </div>
-                        </div>
-                        <div id="Lives" className="tab-pane fade ">
-
-                            <div className="row">
-
-                        {lives.map((item) => (
-                                                        <>
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
-                                                              
-
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                                    <a className="hover-switch"
-                                                                      data-fancybox="video-gallery"
-                                                                      href={item?.path}
-                                                                      data-caption="Video 1">
-                                                                        
-                                                                    <img src={item?.thumbnail} alt="video" className='img'/>
-                                                                    <button className="play-button">▶</button>
-                                                                        
-                                                                    </a>
-                                                                
-                                                                </div>
-
-                                                            </div>
-                                                            </div>
-                                                            </>
-                                                      ))}
-                                                        
-                             </div>
-                                                        
-                        </div>
-                        <div id="Castings" className="tab-pane fade ">
-
-                            <div className="row">
-
-                        {castings.map((item) => (
-                                                        <>
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
-                                                              
-
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                                    <a className="hover-switch"
-                                                                      data-fancybox="video-gallery"
-                                                                      href={item?.path}
-                                                                      data-caption="Video 1">
-                                                                        
-                                                                    <img src={item?.thumbnail} alt="video" className='img'/>
-                                                                    <button className="play-button">▶</button>
-                                                                        
-                                                                    </a>
-                                                                
-                                                                </div>
-
-                                                            </div>
-                                                            </div>
-                                                            </>
-                                                      ))}
-                                                        
-                             </div>
-
-                        </div>
-                        <div id="Defiles" className="tab-pane fade ">
-
-                            <div className="row">
-
-                        {defiles.map((item) => (
-                                                        <>
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
-                                                              
-
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                                    <a className="hover-switch"
-                                                                      data-fancybox="video-gallery"
-                                                                      href={item?.path}
-                                                                      data-caption="Video 1">
-                                                                        
-                                                                    <img src={item?.thumbnail} alt="video" className='img'/>
-                                                                    <button className="play-button">▶</button>
-                                                                        
-                                                                    </a>
-                                                                
-                                                                </div>
-
-                                                            </div>
-                                                            </div>
-                                                            </>
-                                                      ))}
-                                                        
-                             </div>
-
-                        </div>
-                        <div id="Interviews" className="tab-pane fade ">
-
-                            <div className="row">
-
-                        {interviews.map((item) => (
-                                                        <>
-                                                            <div className="box col-lg-2 col-md-4 col-4 aos-init aos-animate" key={item?.id} data-aos="fade-up" data-aos-duration="700">
-                                                              
-
-                                                            <div className="product-card video_card">
-                                                                <div className="product-card-img">
-                                                                    <a className="hover-switch"
-                                                                      data-fancybox="video-gallery"
-                                                                      href={item?.path}
-                                                                      data-caption="Video 1">
-                                                                        
-                                                                    <img src={item?.thumbnail} alt="video" className='img'/>
-                                                                    <button className="play-button">▶</button>
-                                                                        
-                                                                    </a>
-                                                                
-                                                                </div>
-
-                                                            </div>
-                                                            </div>
-                                                            </>
-                                                      ))}
-                                                        
-                             </div>
-
-                        </div>
-
-                    </div>
                 </div>
     </>
   )
